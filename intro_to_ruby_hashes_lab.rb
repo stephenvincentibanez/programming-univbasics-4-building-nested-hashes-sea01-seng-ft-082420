fbcc233f3a0b7b076b06881866825bb943b0e9e8
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-08-06 04:49:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,10 +14,7 @@ def monopoly_with_second_tier
   railroads = {:railroads => {}
   }
  
- railroads[
-   {:pieces => 4},
-   {:rent_in_dollars => {}},
-   {:names => {}}]
+ railroads[:pieces => 4]
 
 end
 
