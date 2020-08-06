a7dcd01bb2609bbccb3cefeb4ae001754aece270
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-08-06 04:57:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,6 +25,14 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
+ railroads = {:railroads => 
+  {:pieces => 4,
+  :rent_in_dollars => {},
+  :names => {}
+}
+}
+end
+
 end
 
 def monopoly_with_fourth_tier
